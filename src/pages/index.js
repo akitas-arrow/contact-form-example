6bfd929a9be98b61546d00b0145eca3c4fabd306
Ayano Taki <ayanobayern2@gmail.com>
2021-02-21 21:50:18 +0900

    Forms設定

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import React from "react"
 import { Formik, Form, Field, ErrorMessage} from 'formik'
 
+const encode = (data) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 export default function Home() {
   return (
     <Formik
@@ -10,10 +15,23 @@ export default function Home() {
         message:'',
         privacy:false,
       }}
-      onSubmit={(values, actions) => {
-        alert(JSON.stringify(values, null, 2));
-        actions.setSubmitting(false);
-      }}
+      onSubmit={
+        (values, actions) => {
+          fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact-form", ...values })
+          })
+          .then(() => {
+            alert('Success');
+            actions.resetForm()
+          })
+          .catch(() => {
+            alert('Error');
+          })
+          .finally(() => actions.setSubmitting(false))
+        }
+      }
       validate={values => {
         const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
         const errors = {};
@@ -33,7 +51,13 @@ export default function Home() {
       }}
     >
       {() => (
-        <Form>
+        <Form
+          name="contact-form"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <Field type="hidden" name="form-name" />
+          <Field name="hidden" name="bot-field" />
           <div>
             <label htmlFor="name">Name: </label>
             <Field name="name" />
