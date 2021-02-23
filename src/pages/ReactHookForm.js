0a02312cca1d3be641af6ae6519893694fe05215
Ayano Taki <ayanobayern2@gmail.com>
2021-02-23 19:58:51 +0900

    入力項目追加

--- src/pages/ReactHookForm.js
+++ src/pages/ReactHookForm.js
@@ -20,9 +20,6 @@ function ReactHookForm() {
       .then(() => navigate("/thanks/"))
       .catch(() => console.log("お問い合わせに失敗しました"));
   };
-  // const onSubmit = data => console.log(data);
-
-  // console.log(watch("name"));
   return (
     <form
       name="contact-form"
@@ -33,21 +30,73 @@ function ReactHookForm() {
       <input type="hidden" name="form-name" />
       <input type="hidden" name="bot-field" />
       <div>
-        <label htmlFor="name">お名前</label>
-        <input name="name" ref={register({ required: true})} />
-        {errors.name && <span>お名前は入力必須項目です</span>}
+        <label>
+          お名前
+          <input name="name" ref={register({ required: 'お名前を入力してください'})} />
+          {errors.name && errors.name.message}
+        </label>
       </div>
       <div>
-        <label htmlFor="furigana">フリガナ</label>
-        <input name="furigana" ref={register({ required: true })} />
+        <label>
+          フリガナ
+          <input
+            name="furigana"
+            ref={register({
+              required: 'お名前を入力してください',
+              pattern:{
+                value: /^[ァ-ヶー　]+$/,
+                message: '全角カタカナで入力してください'
+              }
+            })}
+          />
+          {errors.furigana && errors.furigana.message}
+        </label>
       </div>
       <div>
-        <label htmlFor="address">住所</label>
-        <input name="address" ref={register} />
+        <label>
+          メールアドレス
+          <input name="email" ref={register({ required: true })} />
+          {errors.email && <span>メールアドレスは入力必須項目です</span>}
+        </label>
       </div>
       <div>
-        <label htmlFor="email">メールアドレス</label>
-        <input name="email" ref={register({ required: true })} />
+        <label>
+          電話番号
+          <input name="tel" ref={register({ required: true })} />
+          {errors.tel && <span>電話番号は入力必須項目です</span>}
+        </label>
+      </div>
+      <div>
+        <label>
+          会社名
+          <input name="company-name" ref={register} />
+        </label>
+      </div>
+      <div>
+        <label>
+          郵便番号
+          <input name="postal-code" ref={register} />
+        </label>
+      </div>
+      <div>
+        <label>
+          住所
+          <input name="address" ref={register} />
+        </label>
+      </div>
+      <div>
+        <label>
+          お問い合わせ内容
+          <textarea name="message" ref={register({ required: true })} />
+          {errors.message && <span>お問い合わせ内容は入力必須項目です</span>}
+        </label>
+      </div>
+      <div>
+        <label>
+          同意する
+          <input type="checkbox" name="privacy" ref={register({ required: true })} />
+          {errors.privacy && <span>チェックをしてください</span>}
+        </label>
       </div>
       <input type="submit" />
     </form>
